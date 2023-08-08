cf4a977c22888b7a86f095e75f0b44d97659fac8
Zack  Arno <zackarno@gmail.com>
2023-08-08 09:50:34 -0600

    run styler on code

--- R/email_funcs.R
+++ R/email_funcs.R
@@ -1,27 +1,27 @@
 #' proj_map_viz_dataset
 #'
-#' @param dataset \code{character} name of data set to load 
-#'  options are: 
+#' @param dataset \code{character} name of data set to load
+#'  options are:
 #'    "wca" (West & Central Africa)
 #'    "river"
 #'    "adm1" (Nigeria Admin 1)
 #'
 #' @return spatial data frame containing layers downloaded from hdx
-proj_map_viz_dataset <-  function(dataset){
-  if(dataset=="wca"){
+proj_map_viz_dataset <- function(dataset) {
+  if (dataset == "wca") {
     ret <- pull_dataset("b20cd345-93fb-43bd-9c6e-7bc7d87b63eb") %>%
       get_resource(1) %>%
       read_resource()
   }
-  if(dataset=="river"){
+  if (dataset == "river") {
     ret <- pull_dataset("741c6f20-6956-420d-aae4-37015cdd1ad4") %>%
       get_resource(2) %>%
       # rhdx::get_resource_layers()
-      read_resource(layer="NGA_rvrsl_1m_esri")
+      read_resource(layer = "NGA_rvrsl_1m_esri")
   }
-  if(dataset=="adm1"){
-    ret <- pull_dataset("81ac1d38-f603-4a98-804d-325c658599a3") %>% 
-      get_resource(3) %>% 
+  if (dataset == "adm1") {
+    ret <- pull_dataset("81ac1d38-f603-4a98-804d-325c658599a3") %>%
+      get_resource(3) %>%
       read_resource(layer = "nga_admbnda_adm1_osgof_20190417")
   }
   return(ret)
@@ -39,11 +39,11 @@ proj_map_viz_dataset <-  function(dataset){
 #' library(rhdx)
 #' hdx_map_viz_layers()
 #' }
-hdx_map_viz_layers <- function(){
+hdx_map_viz_layers <- function() {
   list(
-    river= proj_map_viz_dataset(dataset="river"),
-    west_central_africa = proj_map_viz_dataset(dataset="wca"),
-    admin_1 = proj_map_viz_dataset(dataset="adm1")
+    river = proj_map_viz_dataset(dataset = "river"),
+    west_central_africa = proj_map_viz_dataset(dataset = "wca"),
+    admin_1 = proj_map_viz_dataset(dataset = "adm1")
   )
 }
 
@@ -54,17 +54,18 @@ hdx_map_viz_layers <- function(){
 #' @param df data.frame basin_id named "hybas_id"
 #'
 #' @return data.frame w/ specified hybas_ids removed
-filter_basins <- function(df){
-  df %>% 
+
+filter_basins <- function(df) {
+  df %>%
     mutate(
       basin_name = case_when(
         hybas_id == 1040909900 ~ "Benue",
         hybas_id == 1040909890 ~ "Lower Niger",
         hybas_id == 1040022420 ~ "Niger Delta",
         hybas_id == 1040760290 ~ "Upper Niger",
-        .default= NA
-      ),.before=everything()
-    ) %>% 
+        .default = NA
+      ), .before = everything()
+    ) %>%
     filter(!is.na(basin_name))
 }
 
@@ -76,22 +77,24 @@ filter_basins <- function(df){
 #'
 #' @return data.frame with gauges removed that we are not monitoring
 
-filter_gauges <-  function(df, gauge_id=gauge_id){
-  df %>% 
+filter_gauges <- function(df, gauge_id = gauge_id) {
+  df %>%
     filter(
-      (!{{ gauge_id }} %in% c("hybas_1120794570",
-                              "hybas_1120741070",
-                              "hybas_1120946640",
-                              "hybas_1120974450", 
-                              "hybas_1120981190"))
-    ) 
+      (!{{ gauge_id }} %in% c(
+        "hybas_1120794570",
+        "hybas_1120741070",
+        "hybas_1120946640",
+        "hybas_1120974450",
+        "hybas_1120981190"
+      ))
+    )
 }
 
 
 #' accum_pct_gauges_breached
 #'
-#' @param df 
-#' @description 
+#' @param df
+#' @description
 #' Given all gauge data we need to look at each day in the forecast and per basin calculate the total
 #' cumulative number % of gauges that have been breached. The complexity comes from needing to accumulate unique gauge
 #' breachings over days. Created as modular function so that we can use in various places easily while keeping code simple and not needing
@@ -99,16 +102,15 @@ filter_gauges <-  function(df, gauge_id=gauge_id){
 #' @param date \code{character} name of variable containing date
 #' @param lgl_var \code{character} name of variable containing logical value
 #'
-#' @return 
+#' @return
 #' data.frame containing
 #'  'basin_name'
 #'  'date_predict'
 #'  'cum_pct' : % of gauges that have been breached up to that point
-#' @examples
+
 accum_pct_gauges_breached <- function(df,
-                                      date="date_predict", 
-                                      lgl_var ="gte_2_rp"
-){
+                                      date = "date_predict",
+                                      lgl_var = "gte_2_rp") {
   df %>%
     group_by(basin_name, !!sym(date)) %>%
     summarise(
@@ -131,7 +133,7 @@ read_gauge_googlesheets <- function(url = Sys.getenv("GFF_GAUGE_URL")) {
   sn_filt %>%
     map(
       \(sn){
-        cat("read sheet ",sn, "\n")
+        cat("read sheet ", sn, "\n")
         read_sheet(ss = url, sheet = sn) %>%
           clean_names()
       }
@@ -144,14 +146,14 @@ read_gauge_googlesheets <- function(url = Sys.getenv("GFF_GAUGE_URL")) {
 
 #' basin_pal
 #' @description
-#' Helper function to return basin-named palette. Useful to have as function since we can change values here 
+#' Helper function to return basin-named palette. Useful to have as function since we can change values here
 #' and have them change across multiple plots and text elements.
-#' 
+#'
 #' @return named vector. Names are the basin names ('Benue', 'Lower Niger', 'Niger Delta', 'Upper Niger')
-#' @examples \dontrunP
+#' @examples \dontrun{
 #' library(gghdx)
 #' basin_pal()
-
+#' }
 basin_pal <- function() {
   c(
     "tomato-dark",
@@ -173,10 +175,10 @@ basin_pal <- function() {
 
 #' gen_plot_title
 #'
-#' @param df 
+#' @param df
 #' @param date \code{character} column name containing predicted date
 #' @param threshold \code{numeric} threshold
-#' @param basin_palette named vector containing per basin 
+#' @param basin_palette named vector containing per basin
 #'
 #' @return
 #' @export
@@ -189,18 +191,18 @@ gen_plot_title <- function(df,
                            threshold = 0.5,
                            basin_palette = basin_pal()) {
   date_forecast_gen <- min(df[[date]])
-  
+
   basin_df_cum_pct <- accum_pct_gauges_breached(df = df, lgl_var = "gte_2_rp")
-  
+
   basin_first_breach <- basin_df_cum_pct %>%
     group_by(basin_name) %>%
     filter(cum_pct >= threshold) %>%
-    arrange(!!sym(date)) %>% 
+    arrange(!!sym(date)) %>%
     slice(1) %>%
     ungroup()
-  
+
   pre_title <- "Hydrological Forecast"
-  
+
   if (nrow(basin_first_breach) == 0) {
     ret <- pre_title
   } else {
@@ -210,7 +212,7 @@ gen_plot_title <- function(df,
         basin_name_styled = glue("<span style= 'color: {col}'> {basin_name} </span>")
       ) %>%
       pull(basin_name_styled)
-    
+
     # more than 1 basin we need plural
     if (length(basin_vec_syled) > 1) {
       ret <- glue(
@@ -228,44 +230,48 @@ gen_plot_title <- function(df,
 }
 
 
-plot_average_discharge_normalized <-  function(df,
-                                               date="date_predict", 
-                                               threshold=0.8,
-                                               basin_palette=basin_pal()
-){
+plot_average_discharge_normalized <- function(df,
+                                              date = "date_predict",
+                                              threshold = 0.8,
+                                              basin_palette = basin_pal()) {
   # average discharge predicted per day per basin.
-  df_p <- df %>% 
-    group_by(basin_name,!!sym(date)) %>% 
+  df_p <- df %>%
+    group_by(basin_name, !!sym(date)) %>%
     summarise(
-      Q_pct_rp2= mean(Q_pct_rp2,na.rm=T),
-      .groups="drop"
-    ) 
-  
+      Q_pct_rp2 = mean(Q_pct_rp2, na.rm = T),
+      .groups = "drop"
+    )
+
   # plot inputs
-  plot_title <- gen_plot_title(df = df,
-                               date = date,
-                               threshold = threshold,
-                               basin_palette = basin_palette)
+  plot_title <- gen_plot_title(
+    df = df,
+    date = date,
+    threshold = threshold,
+    basin_palette = basin_palette
+  )
   # for setting plot y-axis limits
   Q_pct_max <- max(df_p$Q_pct_rp2)
-  ymax_lim <- ifelse(Q_pct_max<1,1,round(Q_pct_max+0.2,1))
-  
+  ymax_lim <- ifelse(Q_pct_max < 1, 1, round(Q_pct_max + 0.2, 1))
+
   # plot
-  df_p %>% 
+  df_p %>%
     ggplot(
-      aes(x= date_predict,
-          y= Q_pct_rp2,
-          color= basin_name,
-          group=basin_name
+      aes(
+        x = date_predict,
+        y = Q_pct_rp2,
+        color = basin_name,
+        group = basin_name
       )
-    )+
-    geom_smooth(se=FALSE)+
-    scale_color_manual(values = basin_palette)+
-    scale_x_date(breaks = "1 day",
-                 date_labels = "%b %e") +
+    ) +
+    geom_smooth(se = FALSE) +
+    scale_color_manual(values = basin_palette) +
+    scale_x_date(
+      breaks = "1 day",
+      date_labels = "%b %e"
+    ) +
     scale_y_continuous(
       breaks = seq(0, ymax_lim, .2),
-      limits = c(0,ymax_lim),
+      limits = c(0, ymax_lim),
       labels = scales::percent,
     ) +
     labs(
@@ -279,7 +285,6 @@ plot_average_discharge_normalized <-  function(df,
       plot.subtitle = element_markdown(),
       plot.title = element_markdown(size = 15)
     )
-  
 }
 
 
@@ -290,9 +295,9 @@ plot_average_discharge_normalized <-  function(df,
 #' nga_base_map
 #' @description
 #' convenience function to style the base map aesthetics - functinonalized just to simplify pipeline code
-#' @param west_africa_adm0 
+#' @param west_africa_adm0
 #' @param country_fill \code{character} background color of nga
-#' @param surrounding_fill \code{character} background color for surrounding 
+#' @param surrounding_fill \code{character} background color for surrounding
 #'
 #' @return map made with `{tmap}`
 
@@ -300,31 +305,31 @@ nga_base_map <- function(
     west_africa_adm0,
     country_fill = "white",
     surrounding_fill = "lightgrey") {
-  
   # countries of interest
   coi <- west_africa_adm0 %>%
     mutate(
       aoi = ifelse(admin0Pcod == "NG", "aoi", "not_aoi")
     )
-  
+
   # split
   coi_l <- split(coi, coi$aoi)
-  
+
   # map
   tm_shape(coi, bbox = coi_l$aoi) +
     tm_polygons(
       col = "aoi",
       palette = c(country_fill, surrounding_fill),
       legend.show = F
-    )+
-    tm_shape(coi_l$not_aoi)+
-    tm_text(text= "admin0Name",col = "white")+
-    tm_shape(coi_l$aoi)+
-    tm_borders(col = "#414141",lwd = 5,alpha=1)+
-    tm_shape(coi_l$aoi,legend.show=F)+
+    ) +
+    tm_shape(coi_l$not_aoi) +
+    tm_text(text = "admin0Name", col = "white") +
+    tm_shape(coi_l$aoi) +
+    tm_borders(col = "#414141", lwd = 5, alpha = 1) +
+    tm_shape(coi_l$aoi, legend.show = F) +
     tm_borders(
-      col="white",
+      col = "white",
       # lty = 3, # linetype long dash
-      lwd=1,
-      alpha=0.7)
+      lwd = 1,
+      alpha = 0.7
+    )
 }
--- src/gauge_monitoring_email.R
+++ src/gauge_monitoring_email.R
@@ -1,5 +1,3 @@
-
-
 # libs --------------------------------------------------------------------
 # in dedicated monitoring repo should consider setting up w/ {renv}
 # have to load tidyverse packages separate for GHA for some reason
@@ -31,14 +29,14 @@ source("R/email_funcs.R")
 # Get Data ----------------------------------------------------------------
 
 ## Authenticate google APIs ####
-walk(list(drive_auth, gs4_auth),\(f){
+walk(list(drive_auth, gs4_auth), \(f){
   f(
     path = Sys.getenv("GFF_JSON")
   )
 })
 
 drive_dribble <- drive_ls(
-  corpus="user"
+  corpus = "user"
 )
 
 ## Static Layers ####
@@ -50,14 +48,14 @@ L <- hdx_map_viz_layers()
 drive_download(
   as_id("1AsB_Cf9QQb3vkp9ebIFvTPZ5gBYqYRJR"),
   path = basin_fp <- tempfile(fileext = ".rds")
-  )
+)
 
-gdf_basins_poly <- read_rds(basin_fp) %>% 
-  filter_basins() %>% 
+gdf_basins_poly <- read_rds(basin_fp) %>%
+  filter_basins() %>%
   # clip to country
   st_intersection(
-    L$west_central_africa %>% 
-      filter(admin0Pcod=="NG")
+    L$west_central_africa %>%
+      filter(admin0Pcod == "NG")
   )
 
 
@@ -65,91 +63,93 @@ gdf_basins_poly <- read_rds(basin_fp) %>%
 ### Google forecast Workbook ####
 gauge_df_list <- read_gauge_googlesheets(
   url = Sys.getenv("GFF_GAUGE_URL")
-  )
+)
 
 ### Gauge Locations ####
-gdf_gauge <- st_as_sf(gauge_df_list$metadata, 
-                      coords = c("longitude", "latitude"),
-                      crs = 4326) %>% 
-  filter_gauges() %>% 
+gdf_gauge <- st_as_sf(gauge_df_list$metadata,
+  coords = c("longitude", "latitude"),
+  crs = 4326
+) %>%
+  filter_gauges() %>%
   st_join(
-    gdf_basins_poly %>% 
-      select(hybas_id,basin_name)
+    gdf_basins_poly %>%
+      select(hybas_id, basin_name)
   )
 
 ### Discharge Forecasts ####
-df_forecast_long <- gauge_df_list %>% 
-  keep_at(at = ~str_detect(.x,"hybas_")) %>% 
-  bind_rows() %>% 
+df_forecast_long <- gauge_df_list %>%
+  keep_at(at = ~ str_detect(.x, "hybas_")) %>%
+  bind_rows() %>%
   # remove potential duplication issue where two discharge values may get appended to googlesheed w/ same date
   mutate(
-    date= dmy(date),
+    date = dmy(date),
     update_time_utc = ymd_hms(update_time_utc)
-    ) %>% 
-  filter(date==max(date)) %>% 
-  group_by(gauge_id,date) %>%
+  ) %>%
+  filter(date == max(date)) %>%
+  group_by(gauge_id, date) %>%
   filter(
     update_time_utc == max(update_time_utc)
   ) %>%
-  ungroup() %>% 
-  filter_gauges() %>% 
+  ungroup() %>%
+  filter_gauges() %>%
   # add basin informatino to gauge/forecast data
   left_join(
-    gdf_gauge %>% 
+    gdf_gauge %>%
       st_drop_geometry(),
-    by ="gauge_id"
-  ) %>% 
+    by = "gauge_id"
+  ) %>%
   pivot_longer(
     cols = matches("discharge"),
     names_to = "forecast_cat",
     values_to = "Q"
-  ) %>% 
+  ) %>%
   mutate(
-    leadtime = replace_na(parse_number(forecast_cat),0) %>% 
+    leadtime = replace_na(parse_number(forecast_cat), 0) %>%
       suppressWarnings(), # don't need the parse_number warning
     date_predict = date + leadtime,
-    Q_pct_rp2 = Q/x2_years_return_period,
+    Q_pct_rp2 = Q / x2_years_return_period,
     gte_2_rp = Q >= x2_years_return_period,
     gte_5_rp = Q >= x5_years_return_period,
     gte_20_rp = Q >= x20_years_return_period
   )
 
 
-gauge_ids_breaching <- df_forecast_long %>% 
-  filter(gte_2_rp) %>% 
-  distinct(gauge_id) %>% 
+gauge_ids_breaching <- df_forecast_long %>%
+  filter(gte_2_rp) %>%
+  distinct(gauge_id) %>%
   pull(gauge_id)
 
-gdf_gauge_pts <- gdf_gauge %>% 
+gdf_gauge_pts <- gdf_gauge %>%
   mutate(
-    lgl_gauge_status =gauge_id %in% gauge_ids_breaching,
-    `Gauge Status` =if_else(lgl_gauge_status,"Threshold Exceeded","Below Threshold"),
-    aes_dot_size =if_else(lgl_gauge_status,0.01,.005),
-    aes_dot_alpha = if_else(lgl_gauge_status,1,0.5)
+    lgl_gauge_status = gauge_id %in% gauge_ids_breaching,
+    `Gauge Status` = if_else(lgl_gauge_status, "Threshold Exceeded", "Below Threshold"),
+    aes_dot_size = if_else(lgl_gauge_status, 0.01, .005),
+    aes_dot_alpha = if_else(lgl_gauge_status, 1, 0.5)
   )
 
 # Aggregate Alert Status to basin level.
 df_basin_alert_status <- accum_pct_gauges_breached(
   df = df_forecast_long,
-  date="date_predict",
+  date = "date_predict",
   lgl_var = "gte_2_rp"
-) %>% 
-  group_by(basin_name) %>% 
+) %>%
+  group_by(basin_name) %>%
   slice_max(
     order_by = cum_pct,
     with_ties = F
-  ) %>% 
-  ungroup() %>% 
+  ) %>%
+  ungroup() %>%
   mutate(
-    `Basin Alert Status` = if_else(cum_pct >= 0.8 ,"Warning","No Warning")
+    `Basin Alert Status` = if_else(cum_pct >= 0.8, "Warning", "No Warning")
   )
 
-gdf_basin_alert_poly <- gdf_basins_poly %>% 
+gdf_basin_alert_poly <- gdf_basins_poly %>%
   left_join(
-    df_basin_alert_status,by="basin_name"
+    df_basin_alert_status,
+    by = "basin_name"
   )
 
-gdf_basin_alert_lines <- st_cast(gdf_basin_alert_poly,"MULTILINESTRING")
+gdf_basin_alert_lines <- st_cast(gdf_basin_alert_poly, "MULTILINESTRING")
 
 
 # Alert Map ---------------------------------------------------------------
@@ -158,55 +158,61 @@ m_basin_alerts <- nga_base_map(
   west_africa_adm0 = L$west_central_africa,
   country_fill = "white",
   surrounding_fill = hdx_colors()["gray-dark"]
-)+
+) +
   tm_shape(
     gdf_basin_alert_poly
-    
-  )+
-  tm_polygons(col= "Basin Alert Status",
-              palette=c(
-                `No Warning`= hdx_colors()["mint-ultra-light"],
-                `Warning` = hdx_colors()["tomato-hdx"]
-              ) ,
-              alpha = 0.5,
-              border.col = NULL 
-  )+
+  ) +
+  tm_polygons(
+    col = "Basin Alert Status",
+    palette = c(
+      `No Warning` = hdx_colors()["mint-ultra-light"],
+      `Warning` = hdx_colors()["tomato-hdx"]
+    ),
+    alpha = 0.5,
+    border.col = NULL
+  ) +
   tm_shape(
     gdf_basin_alert_lines,
-    legend.show=F
-  )+
-  tm_lines(col = "Basin Alert Status",
-           legend.col.show = F,
-           palette=c(hdx_colors()["mint-hdx"],
-                     hdx_colors()["tomato-dark"]),
-           lwd=4,alpha = 0.3
-  )+
-  tm_shape(L$admin_1)+
-  tm_borders(
-    col=hdx_colors()["gray-dark"],
-    alpha = 0.2
-  )+
-  tm_shape(L$river)+
+    legend.show = F
+  ) +
   tm_lines(
-    col=hdx_colors()["sapphire-hdx" ],
-    lwd=3)+
+    col = "Basin Alert Status",
+    legend.col.show = F,
+    palette = c(
+      hdx_colors()["mint-hdx"],
+      hdx_colors()["tomato-dark"]
+    ),
+    lwd = 4, alpha = 0.3
+  ) +
+  tm_shape(L$admin_1) +
+  tm_borders(
+    col = hdx_colors()["gray-dark"],
+    alpha = 0.2
+  ) +
+  tm_shape(L$river) +
+  tm_lines(
+    col = hdx_colors()["sapphire-hdx"],
+    lwd = 3
+  ) +
   # gauge locations
   tm_shape(gdf_gauge_pts,
-           legend.show=F
-  )+
+    legend.show = F
+  ) +
   tm_dots(
-    col="Gauge Status",
-    size= 0.2,
-    palette=c("#25252533","black"),
-    legend.size.show=F
-  )+
-  tm_shape(gdf_basin_alert_poly)+
-  tm_text(text = "basin_name",shadow = T)+
-  tm_layout(main.title.size = 0.5,
-            title.size = 0.5,
-            legend.text.size = 0.5,
-            legend.title.size = 0.7,
-            bg.color = "lightblue")
+    col = "Gauge Status",
+    size = 0.2,
+    palette = c("#25252533", "black"),
+    legend.size.show = F
+  ) +
+  tm_shape(gdf_basin_alert_poly) +
+  tm_text(text = "basin_name", shadow = T) +
+  tm_layout(
+    main.title.size = 0.5,
+    title.size = 0.5,
+    legend.text.size = 0.5,
+    legend.title.size = 0.7,
+    bg.color = "lightblue"
+  )
 
 
 
@@ -217,11 +223,11 @@ p_discharge <- plot_average_discharge_normalized(
   date = "date_predict",
   threshold = 0.8,
   basin_palette = basin_pal()
-    )
+)
 
 txt_warning_status <- ifelse(
-  any(df_basin_alert_status$`Basin Alert Status`=="Warning"),"Warning","No Warning"
-  )
+  any(df_basin_alert_status$`Basin Alert Status` == "Warning"), "Warning", "No Warning"
+)
 
 
 # config email ------------------------------------------------------------
@@ -229,8 +235,10 @@ txt_warning_status <- ifelse(
 date_prediction_made <- df_forecast_long$date %>% unique()
 dt_made_chr <- trimws(format(as_date(date_prediction_made), "%e %B %Y"))
 # Generate conditional email subject
-subj_email <-  paste0("Nigeria Riverine Flood Monitoring: ",
-                      dt_made_chr)
+subj_email <- paste0(
+  "Nigeria Riverine Flood Monitoring: ",
+  dt_made_chr
+)
 
 # will need to write code w/ `{googledrive}` package to access from service account/GH Action runner
 
@@ -242,8 +250,8 @@ drive_download(
 
 
 email_receps_df <- read_csv(email_receps_fp)
-email_to <- email_receps_df %>% 
-  filter(to) %>% 
+email_to <- email_receps_df %>%
+  filter(to) %>%
   pull(email_address)
 
 
@@ -268,6 +276,3 @@ render_email(
     subject = subj_email,
     credentials = email_creds
   )
-
-
-
